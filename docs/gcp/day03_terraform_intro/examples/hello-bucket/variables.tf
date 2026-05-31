e7977c4de1735de708cb90ea667f260bf220cf04
nobumitsu_masugata <nobumitsu.masugata@ysinc.co.jp>
2026-05-31 20:30:20 +0900

    feat: DAY3の内容を拡充

--- docs/gcp/day03_terraform_intro/examples/hello-bucket/variables.tf
+++ docs/gcp/day03_terraform_intro/examples/hello-bucket/variables.tf
@@ -1,6 +1,6 @@
 variable "project_id" {
   description = "GCPプロジェクトID" # terraform plan時に表示される説明文
-  type        = string             # 文字列型（必須入力）
+  type        = string        # 文字列型（必須入力）
 }
 
 variable "region" {
